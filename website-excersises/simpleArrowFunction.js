426caffb56a3ac4c47ad019825a83884983b8088
Raghulsankar <raghulsankara04@gmail.com>
2026-01-05 10:40:34 +0530

    arrow function

---
+++ website-excersises/simpleArrowFunction.js
@@ -0,0 +1,11 @@
+// Create an arrow function that adds two numbers
+const add = (num1, num2) => {
+  return num1 + num2;
+}
+// Your code here
+
+
+console.log(add(5, 3));
+console.log(add(10, 7));
+console.log(add(0, 0));
+console.log(add(-5, 5));
