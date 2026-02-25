d28ae3e2100e09fb46dceba2c4a2e45465e6bb5d
Raghulsankar <raghulsankara04@gmail.com>
2026-02-25 23:15:35 +0530

    number formatting in python

---
+++ Python/Day-3/number_formatting.py
@@ -0,0 +1,16 @@
+followers = 2000000
+
+print(f"The count is: {followers:,}")
+
+
+height = 175.626
+
+print(f"The height is: {height:.2f}")
+print(f"The height is: {height:.0f}")  # rounds
+print(f"The height is: {height:.4f}")
+
+money = 2000000.8992
+print(f"The count is: {money:,.2f}")
+
+
+
